1b59e60a8693c8c2ce1bbc58ec827606091f2bb7
Szczepan Ćwikliński <sec@thinkcode.pl>
2024-12-01 11:09:54 +0100

    Day 01

--- aoc2024/Code/Day01.cs
+++ aoc2024/Code/Day01.cs
@@ -2,13 +2,24 @@ namespace aoc2024.Code;
 
 internal class Day01 : BaseDay
 {
-    protected override object Part1()
+    int Solver(Func<(List<int> One, List<int> Two), int> solver)
     {
-        throw new NotImplementedException();
+        var one = new List<int>();
+        var two = new List<int>();
+
+        foreach (var item in ReadAllLinesSplit(" ", true))
+        {
+            one.Add(int.Parse(item[0]));
+            two.Add(int.Parse(item[1]));
+        }
+
+        one.Sort();
+        two.Sort();
+
+        return solver((one, two));
     }
 
-    protected override object Part2()
-    {
-        throw new NotImplementedException();
-    }
+    protected override object Part1() => Solver(d => Enumerable.Zip(d.One, d.Two).Select(zip => Math.Abs(zip.First - zip.Second)).Sum());
+
+    protected override object Part2() => Solver(d => d.One.Select(x => x * d.Two.Count(y => y == x)).Sum());
 }
