bf209cdf643d21f63c4017510bd95e1c4bfd5fa3
Szczepan Ćwikliński <sec@thinkcode.pl>
2024-12-02 09:01:49 +0100

    Day 02

--- aoc2024/Code/Day02.cs
+++ aoc2024/Code/Day02.cs
@@ -2,13 +2,50 @@ namespace aoc2024.Code;
 
 internal class Day02 : BaseDay
 {
-    protected override object Part1()
+    static bool IsSafe(List<int> items)
     {
-        throw new NotImplementedException();
+        var sign = Math.Sign(items[0] - items[1]);
+
+        for (int i = 0; i < items.Count - 1; i++)
+        {
+            var diff = items[i] - items[i + 1];
+
+            if (Math.Sign(diff) != sign)
+            {
+                return false;
+            }
+
+            if (Math.Abs(diff) is < 1 or > 3)
+            {
+                return false;
+            }
+        }
+        return true;
     }
+
+    protected override object Part1() => ReadAllLinesSplit(" ", true).Count(x => IsSafe(x.Select(int.Parse).ToList()));
 
     protected override object Part2()
     {
-        throw new NotImplementedException();
+        var total = 0;
+
+        foreach (var line in ReadAllLinesSplit(" ", true))
+        {
+            var items = line.Select(int.Parse).ToList();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                var level = items.ToList();
+                level.RemoveAt(i);
+
+                if (IsSafe(level))
+                {
+                    total++;
+                    break;
+                }
+            }
+        }
+
+        return total;
     }
 }
