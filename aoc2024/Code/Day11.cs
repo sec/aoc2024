e3b3478b0f407a8c7f886ad3bd22eb1736ce554f
Szczepan Ćwikliński <sec@thinkcode.pl>
2024-12-11 13:04:02 +0100

    Day 11

--- aoc2024/Code/Day11.cs
+++ aoc2024/Code/Day11.cs
@@ -2,13 +2,47 @@ namespace aoc2024.Code;
 
 internal class Day11 : BaseDay
 {
-    protected override object Part1()
+    readonly static Dictionary<(long, int), long> _cache = [];
+
+    static long CountBlink(long stone, int times)
     {
-        throw new NotImplementedException();
+        if (_cache.TryGetValue((stone, times), out var cached))
+        {
+            return cached;
+        }
+
+        while (times-- > 0)
+        {
+            if (stone == 0)
+            {
+                stone = 1;
+                continue;
+            }
+
+            if (((int) Math.Log10(stone) + 1) % 2 == 0)
+            {
+                var str = stone.ToString();
+                var left = long.Parse(str[..(str.Length / 2)]);
+                var right = long.Parse(str[(str.Length / 2)..]);
+
+                var result1 = CountBlink(left, times);
+                var result2 = CountBlink(right, times);
+
+                _cache[(left, times)] = result1;
+                _cache[(right, times)] = result2;
+
+                return result1 + result2;
+            }
+
+            stone *= (2 << 10) - 24;
+        }
+
+        return 1;
     }
 
-    protected override object Part2()
-    {
-        throw new NotImplementedException();
-    }
+    static long Blink(List<long> stones, int times) => stones.Select(s => CountBlink(s, times)).Sum();
+
+    protected override object Part1() => Blink(ReadAllTextSplit(" ").Select(long.Parse).ToList(), 25);
+
+    protected override object Part2() => Blink(ReadAllTextSplit(" ").Select(long.Parse).ToList(), 75);
 }
