416ed940b44a67409d87741de836afea568bca77
Szczepan Ćwikliński <sec@thinkcode.pl>
2024-12-13 11:02:41 +0100

    Day 13

--- aoc2024/Code/Day13.cs
+++ aoc2024/Code/Day13.cs
@@ -1,14 +1,55 @@
 namespace aoc2024.Code;
 
-internal class Day13 : BaseDay
+internal partial class Day13 : BaseDay
 {
-    protected override object Part1()
+    [GeneratedRegex(@"(\d+).+?(\d+)")]
+    private static partial Regex TwoNumbers();
+
+    record struct Button(long X, long Y);
+
+    record struct Arcade(Button A, Button B, Button Prize);
+
+    static long Play(Arcade arcade, long add)
     {
-        throw new NotImplementedException();
+        var resultX = arcade.Prize.X + add;
+        var resultY = arcade.Prize.Y + add;
+
+        var w = arcade.A.X * arcade.B.Y - arcade.B.X * arcade.A.Y;
+        var wx = resultX * arcade.B.Y - resultY * arcade.B.X;
+        var wy = arcade.A.X * resultY - resultX * arcade.A.Y;
+
+        var pressA = wx / w;
+        var pressB = wy / w;
+
+        if (arcade.A.X * pressA + arcade.B.X * pressB == resultX && arcade.A.Y * pressA + arcade.B.Y * pressB == resultY)
+        {
+            return pressA * 3 + pressB;
+        }
+        return 0;
     }
 
-    protected override object Part2()
+    long PlayArcade(long add)
     {
-        throw new NotImplementedException();
+        var arcades = new List<Arcade>();
+        var r = TwoNumbers();
+        var data = ReadAllLines(true);
+        for (int i = 0; i < data.Length; i += 3)
+        {
+            var a = r.Match(data[i]);
+            var b = r.Match(data[i + 1]);
+            var p = r.Match(data[i + 2]);
+
+            arcades.Add(new(
+                new(long.Parse(a.Groups[1].Value), long.Parse(a.Groups[2].Value)),
+                new(long.Parse(b.Groups[1].Value), long.Parse(b.Groups[2].Value)),
+                new(long.Parse(p.Groups[1].Value), long.Parse(p.Groups[2].Value))
+            ));
+        }
+
+        return arcades.Sum(s => Play(s, add));
     }
+
+    protected override object Part1() => PlayArcade(0);
+
+    protected override object Part2() => PlayArcade(10_000_000_000_000);
 }
