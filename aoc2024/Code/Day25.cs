884ba3634b922890303cf1cf3de71a8e47da1db3
Szczepan Ćwikliński <sec@thinkcode.pl>
2024-12-25 16:20:13 +0100

    Day 25

--- aoc2024/Code/Day25.cs
+++ aoc2024/Code/Day25.cs
@@ -2,13 +2,27 @@ namespace aoc2024.Code;
 
 internal class Day25 : BaseDay
 {
-    protected override object Part1()
+    record FivePin
     {
-        throw new NotImplementedException();
+        readonly BitArray _bits;
+
+        public FivePin(string[] map) => _bits = new BitArray(map.SelectMany(s => s.Select(c => c == '#')).ToArray());
+
+        public bool Match(FivePin other) => !new BitArray(_bits).And(other._bits).HasAnySet();
     }
 
-    protected override object Part2()
+    protected override object Part1()
     {
-        throw new NotImplementedException();
+        var locks = new List<FivePin>();
+        var keys = new List<FivePin>();
+
+        foreach (var map in ReadAllLines(true).Chunk(7))
+        {
+            (map[0] == "#####" ? locks : keys).Add(new FivePin(map));
+        }
+
+        return keys.Select(k => locks.Where(l => l.Match(k)).Count()).Sum();
     }
+
+    protected override object Part2() => 2024;
 }
